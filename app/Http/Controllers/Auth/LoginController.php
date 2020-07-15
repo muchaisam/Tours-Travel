3fb91c3a7a68ce091fd51e2fe389d99550c37fd5
muchaisam <samson.muchai@strathmore.edu>
2020-07-15 09:15:37 +0300

    updated login and register pages

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+  
+namespace App\Http\Controllers\Auth;
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+  
+class LoginController extends Controller
+{
+  
+    use AuthenticatesUsers;
+    
+    protected $redirectTo = '/home';
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+  
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function login(Request $request)
+    {   
+        $input = $request->all();
+  
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+  
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        if(auth()->attempt(array($fieldType => $input['username'], 'password' => $input['password'])))
+        {
+            return redirect()->route('home');
+        }else{
+            return redirect()->route('login')
+                ->with('error','Email-Address And Password Are Wrong.');
+        }
+          
+    }
+}
