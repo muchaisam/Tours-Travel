0d06ecdd271a45c6dd83d6a65d0c47e33bb703fc
muchaisam <samson.muchai@strathmore.edu>
2020-08-01 09:41:02 +0300

    tag selector

--- resources/views/Destinations/create.blade.php
+++ resources/views/Destinations/create.blade.php
@@ -74,7 +74,7 @@
             <div class="form-group">
                 <label for="tags">Tags</label>
 
-                <select name="tags" id="tags" class="form-control" multiple>
+                <select name="tags" id="tags" class="form-control tags-selector" multiple>
                     @foreach ($tags as $tag )
                     <option value="{{$tag->id}}" 
                     @if(isset($destination)) 
@@ -108,16 +108,22 @@
 @section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.3/trix.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 <script>
     flatpickr('#published_at',{
         enableTime:true
     })
+
+    $(document).ready(function() {
+    $('.tags-selector').select2();
+});
 </script>
 @endsection
 
 @section('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.3/trix.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 
 
 @endsection
