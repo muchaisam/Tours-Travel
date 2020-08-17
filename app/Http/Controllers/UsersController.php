6354b6bb71b558d09e50c431e363d9680a4eecd3
muchaisam <samson.muchai@strathmore.edu>
2020-08-17 19:33:13 +0300

    updating profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\Users\UpdateProfileRequest;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -9,6 +11,25 @@ class UsersController extends Controller
     public function index()
     {
         return view('users.index')->with('users', User::all());
+    }
+
+    public function edit()
+    {
+        return view('users.edit')->with('user', auth()->user());
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $user=auth()->user();
+
+        $user->update([
+            'name'=>$request->name,
+            'about'=>$request->about
+        ]);
+
+        session()->flash('success', 'User profile updated successfully');
+
+        return redirect()->back();
     }
 
     public function makeAdmin(User $user)
