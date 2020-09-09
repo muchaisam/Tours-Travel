cb2133f107d9a5fdeebca476971e4cca0b64b83e
muchaisam <samson.muchai@strathmore.edu>
2020-09-09 18:31:09 +0300

    request files blogs

---
+++ app/Http/Requests/Blog/CreateBlogRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Blog;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateBlogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' =>'required|unique:Destinations',
+            'description' =>'required',
+            'image' => 'required|Image',
+            'content' =>'required',
+            'category'=>'required'
+        ];
+    }
+}
