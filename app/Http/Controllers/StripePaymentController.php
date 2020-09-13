f4d18776ee0f8920502363941f17945e7e861b97
muchaisam <samson.muchai@strathmore.edu>
2020-09-13 21:02:31 +0300

    Stripecontroller

---
+++ app/Http/Controllers/StripePaymentController.php
@@ -0,0 +1,42 @@
+<?php
+   
+namespace App\Http\Controllers;
+   
+use Illuminate\Http\Request;
+use Illuminate\Session;
+use Stripe;
+   
+class StripePaymentController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripe()
+    {
+        return view('stripe');
+    }
+  
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create ([
+                "amount" => 100 * 100,
+                "currency" => "Ksh",
+                "source" => $request->stripeToken,
+                "description" => "Muchai Sam." 
+        ]);
+  
+        Session::flash('success', 'Your Payment was successful!');
+
+
+          
+        return redirect(route('packages'));
+    }
+}
