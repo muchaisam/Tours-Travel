49f8fe062dc9e4e33ce45731b8b79d5bc8fe54c1
muchaisam <samson.muchai@strathmore.edu>
2020-09-24 16:16:36 +0300

    functionality of the page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,208 +1,263 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Safari</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+<head>
+  <title>Safari</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
+  <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+  <link rel="stylesheet" href="css/animate.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
 
-    
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-	    <div class="container">
-	      <a class="navbar-brand" href="">Safari<span>Travel Agency</span></a>
-	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
-	      </button>
+  <link rel="stylesheet" href="css/aos.css">
 
-	      <div class="collapse navbar-collapse" id="ftco-nav">
-	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item"><a href="" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="{{route('packages')}}" class="nav-link">Destination</a></li>
-            <li class="nav-item"><a href="{{route('blog')}}" class="nav-link">Blog</a></li>
-            <li class="nav-item"><a href="{{route('about')}}" class="nav-link">About</a></li>
-	          <li class="nav-item active"><a href="{{route('contact')}}" class="nav-link">Contact</a></li>
-	          <li class="nav-item cta"><a href="{{route('login')}}" class="nav-link">Login</a></li>
+  <link rel="stylesheet" href="css/ionicons.min.css">
 
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
-    <!-- END nav -->
-    
-    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/destination-12.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
-          <div class="col-md-9 ftco-animate pb-5 text-center">
-            <h1 class="mb-3 bread">Contact Us</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Contact us <i class="ion-ios-arrow-forward"></i></span></p>
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+  <link rel="stylesheet" href="css/flaticon.css">
+  <link rel="stylesheet" href="css/icomoon.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="">Safari<span>Travel Agency</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+        aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="{{route('packages')}}" class="nav-link">Destination</a></li>
+          <li class="nav-item"><a href="{{route('blog')}}" class="nav-link">Blog</a></li>
+          <li class="nav-item"><a href="{{route('about')}}" class="nav-link">About</a></li>
+          <li class="nav-item active"><a href="{{route('contact')}}" class="nav-link">Contact</a></li>
+          <li class="nav-item cta"><a href="{{route('login')}}" class="nav-link">Login</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- END nav -->
+
+  <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/destination-12.jpg');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
+        <div class="col-md-9 ftco-animate pb-5 text-center">
+          <h1 class="mb-3 bread">Contact Us</h1>
+          <p class="breadcrumbs"><span class="mr-2"><a href="">Home <i class="ion-ios-arrow-forward"></i></a></span>
+            <span>Contact us <i class="ion-ios-arrow-forward"></i></span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section ftco-no-pb contact-section">
+    <div class="container">
+      <div class="row d-flex contact-info">
+        <div class="col-md-3 d-flex">
+          <div class="align-self-stretch box p-4 text-center">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <span class="icon-map-signs"></span>
+            </div>
+            <h3 class="mb-2">Address</h3>
+            <p>Ole Sangale Road, off
+              Langata Road, in Madaraka Estate, Nairobi, Kenya.</p>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex">
+          <div class="align-self-stretch box p-4 text-center">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <span class="icon-phone2"></span>
+            </div>
+            <h3 class="mb-2">Contact Number</h3>
+            <p><a href="tel://1234567920">+254712345678</a></p>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex">
+          <div class="align-self-stretch box p-4 text-center">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <span class="icon-paper-plane"></span>
+            </div>
+            <h3 class="mb-2">Email Address</h3>
+            <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+          </div>
+        </div>
+        <div class="col-md-3 d-flex">
+          <div class="align-self-stretch box p-4 text-center">
+            <div class="icon d-flex align-items-center justify-content-center">
+              <span class="icon-globe"></span>
+            </div>
+            <h3 class="mb-2">Website</h3>
+            <p><a href="#">safari.com</a></p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="ftco-section ftco-no-pb contact-section">
-      <div class="container">
-        <div class="row d-flex contact-info">
-          <div class="col-md-3 d-flex">
-          	<div class="align-self-stretch box p-4 text-center">
-          		<div class="icon d-flex align-items-center justify-content-center">
-          			<span class="icon-map-signs"></span>
-          		</div>
-          		<h3 class="mb-2">Address</h3>
-	            <p>Ole Sangale Road, off
-									Langata Road, in Madaraka Estate, Nairobi, Kenya.</p>
-	          </div>
+  <section class="ftco-section contact-section">
+    <div class="container">
+      <div class="row block-9">
+        <div class="col-md-6 order-md-last d-flex">
+          <!-- Success message -->
+          @if(Session::has('success'))
+          <div class="alert alert-success">
+            {{Session::get('success')}}
           </div>
-          <div class="col-md-3 d-flex">
-          	<div class="align-self-stretch box p-4 text-center">
-          		<div class="icon d-flex align-items-center justify-content-center">
-          			<span class="icon-phone2"></span>
-          		</div>
-          		<h3 class="mb-2">Contact Number</h3>
-	            <p><a href="tel://1234567920">+254712345678</a></p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="align-self-stretch box p-4 text-center">
-          		<div class="icon d-flex align-items-center justify-content-center">
-          			<span class="icon-paper-plane"></span>
-          		</div>
-          		<h3 class="mb-2">Email Address</h3>
-	            <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="align-self-stretch box p-4 text-center">
-          		<div class="icon d-flex align-items-center justify-content-center">
-          			<span class="icon-globe"></span>
-          		</div>
-          		<h3 class="mb-2">Website</h3>
-	            <p><a href="#">safari.com</a></p>
-	          </div>
-          </div>
+          @endif
+          <form action="#" method="POST" action="{{route('contact.store')}}" class="bg-light p-5 contact-form">
+            @csrf
+            <div class="form-group">
+              <label>Name</label>
+              <input type="text" class="form-control {{ $errors->has('name') ? 'error' : '' }}" name="name" id="name">
+
+              <!-- Error -->
+              @if ($errors->has('name'))
+              <div class="error">
+                {{ $errors->first('name') }}
+              </div>
+              @endif
+            </div>
+
+            <div class="form-group">
+              <label>Email</label>
+              <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email"
+                id="email">
+
+              @if ($errors->has('email'))
+              <div class="error">
+                {{ $errors->first('email') }}
+              </div>
+              @endif
+            </div>
+
+            
+
+            <div class="form-group">
+              <label>Subject</label>
+              <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}" name="subject"
+                id="subject">
+
+              @if ($errors->has('subject'))
+              <div class="error">
+                {{ $errors->first('subject') }}
+              </div>
+              @endif
+            </div>
+
+            <div class="form-group">
+              <label>Message</label>
+              <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" id="message"
+                rows="4"></textarea>
+
+              @if ($errors->has('message'))
+              <div class="error">
+                {{ $errors->first('message') }}
+              </div>
+              @endif
+            </div>
+            <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
+          </form>
+
+        </div>
+
+        <div class="col-md-6 d-flex">
+          <div id="map" class="bg-white"></div>
         </div>
       </div>
-    </section>
-			
-		<section class="ftco-section contact-section">
-      <div class="container">
-        <div class="row block-9">
-          <div class="col-md-6 order-md-last d-flex">
-            <form action="#" class="bg-light p-5 contact-form">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Name">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Email">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Subject">
-              </div>
-              <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-              </div>
-              <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
-              </div>
-            </form>
-          
-          </div>
+    </div>
+  </section>
 
-          <div class="col-md-6 d-flex">
-          	<div id="map" class="bg-white"></div>
+  <footer class="ftco-footer bg-bottom" style="background-image: url(images/footer-bg.jpg);">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Safari</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+              blind texts.</p>
+            <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+              <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+            </ul>
           </div>
         </div>
-      </div>
-    </section>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4 ml-md-5">
+            <h2 class="ftco-heading-2">Categories</h2>
+            @foreach ($categories as $category)
+            <div class="col-6">
+              <a href="#">
+                {{$category->name}}
+              </a>
 
-    <footer class="ftco-footer bg-bottom" style="background-image: url(images/footer-bg.jpg);">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Safari</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+            </div>
+            @endforeach
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Tags</h2>
+            @foreach ($tags as $tag)
+            <div class="col-6">
+              <a href="#">
+                {{$tag->name}}
+              </a>
+            </div>
+            @endforeach
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Have a Questions?</h2>
+            <div class="block-23 mb-3">
+              <ul>
+                <li><span class="icon icon-map-marker"></span><span class="text">Ole Sangale Road, off
+                    Langata Road, in Madaraka Estate, Nairobi, Kenya.</span></li>
+                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+254712345678</span></a></li>
+                <li><a href="#"><span class="icon icon-envelope"></span><span
+                      class="text">info@yourdomain.com</span></a></li>
               </ul>
             </div>
           </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Categories</h2>
-					@foreach ($categories as $category)
-					<div class="col-6">
-						<a href="#">
-							{{$category->name}} 
-            </a>
-        
-					</div>
-					@endforeach
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Tags</h2>
-					@foreach ($tags as $tag)
-					<div class="col-6">
-						<a href="#">
-							{{$tag->name}}
-						</a>
-					</div>
-					@endforeach
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">Ole Sangale Road, off
-									Langata Road, in Madaraka Estate, Nairobi, Kenya.</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+254712345678</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-	              </ul>
-	            </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
-          </div>
         </div>
       </div>
-    </footer>
-    
-  
+      <div class="row">
+        <div class="col-md-12 text-center">
+
+          <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>
+              document.write(new Date().getFullYear());
+            </script> All rights reserved </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
 
   <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#F96D00" /></svg></div>
 
 
   <script src="js/jquery.min.js"></script>
@@ -218,9 +273,11 @@
   <script src="js/jquery.animateNumber.min.js"></script>
   <script src="js/bootstrap-datepicker.js"></script>
   <script src="js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+  </script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-    
-  </body>
+
+</body>
+
 </html>
