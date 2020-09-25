ca03ad6c3abf159f6615427a186b378425e02ff6
wngeti85-creator <63838012+wngeti85-creator@users.noreply.github.com>
2020-09-25 20:31:08 +0300

    sending emails

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\HP\Tours-Travel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\HP\Tours-Travel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Ngeti Mdindi\Tours-Travel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Ngeti Mdindi\Tours-Travel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/Email/demoEmail.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Your Travel Details
+
+Thank you for booking your trip to Paris.
+
+@component('mail::button', ['url' => ''])
+#1b27q85
+@endcomponent
+
+Thanks, Safari.<br>
+{{ config('Safari') }}
+@endcomponent
