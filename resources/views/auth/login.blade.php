f6ec35e270ecda445035b43f1b1a43bd746d0dce
muchaisam <samson.muchai@strathmore.edu>
2020-09-26 11:22:32 +0300

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username Or Email</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">Email</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autofocus>
