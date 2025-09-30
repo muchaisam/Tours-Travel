0697bb651e5b03a985c313d1f35e29bf4d433993
muchaisam <samson.muchai@strathmore.edu>
2025-09-30 13:28:04 +0300

    Add DatabaseSeeder to initialize database with Users, Destinations, and Blogs

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+        $this->call(DestinationsTableSeeder::class);
+        $this->call(BlogsTableSeeder::class);
+    }
+}
