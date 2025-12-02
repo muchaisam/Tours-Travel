cfa85adc47312db3a139a15a1ecf3ec44b813873
muchaisam <samson.muchai@strathmore.edu>
2025-12-02 12:27:57 +0300

    fix: update homepage to use shared footer partial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -822,76 +822,8 @@
 	</div>
 </section>--}}
 
-<footer class="ftco-footer bg-bottom footer-bg">
-	<div class="container">
-		<div class="row mb-5">
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Safari</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
-						live the blind texts.</p>
-					<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-						<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-						<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-						<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4 ml-md-5">
-					<h2 class="ftco-heading-2">Categories</h2>
-					@foreach ($categories as $category)
-					<div class="col-6">
-						<a href="#">
-							{{$category->name}}
-						</a>
-					</div>
-					@endforeach
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Tags</h2>
-					@foreach ($tags as $tag)
-					<div class="col-6">
-						<a href="#">
-							{{$tag->name}}
-						</a>
-					</div>
-					@endforeach
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Have any Questions?</h2>
-					<div class="block-23 mb-3">
-						<ul>
-							<li><span class="icon icon-map-marker"></span><span class="text">Ole Sangale Road, off
-									Langata Road, in Madaraka Estate, Nairobi, Kenya.</span></li>
-							<li><a href="#"><span class="icon icon-phone"></span><span
-										class="text">+254712345678</span></a></li>
-							<li><a href="#"><span class="icon icon-envelope"></span><span
-										class="text">info@yourdomain.com</span></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 text-center">
-
-				<p>
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					Copyright &copy;<script>
-						document.write(new Date().getFullYear());
-					</script> All rights reserved
-				</p>
-			</div>
-		</div>
-	</div>
-</footer>
-
-
+<!-- Include Modern Footer -->
+@include('partials.footer')
 
 <!-- loader -->
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
