f67bc13e11c9303522b64d2bdf2b8a3e8bf395b4
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 11:04:01 +0300

    refactor: fix formatting and organize imports in PostController

--- app/Http/Controllers/Packages/PostController.php
+++ app/Http/Controllers/Packages/PostController.php
@@ -3,17 +3,16 @@
 namespace App\Http\Controllers\Packages;
 
 use App\Category;
-use App\Tag;
 use App\Destinations;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Tag;
 
 class Postcontroller extends Controller
 {
     public function show(Destinations $destination)
     {
         return view('desti.show')->with('destinations', $destination)
-        ->with('categories', Category::all())
-        ->with('tags', Tag::all());
+            ->with('categories', Category::all())
+            ->with('tags', Tag::all());
     }
 }
