3b0bfcc46d3201b08e80c64bf70de6fff8bd5a42
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 11:06:06 +0300

    refactor: improve code formatting in VerifyCategoriesCount middleware

--- app/Http/Middleware/VerifyCategoriesCount.php
+++ app/Http/Middleware/VerifyCategoriesCount.php
@@ -11,16 +11,16 @@ class VerifyCategoriesCount
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(Category::all()->count()==0){
+        if (Category::all()->count() == 0) {
             session()->flash('error', 'Create categories before adding a destination.');
 
             return redirect(route('categories.create'));
         }
+
         return $next($request);
     }
 }
