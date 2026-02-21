b7a95b1eceec759ee383996de041fc13198f4505
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 11:11:59 +0300

    refactor: remove unused Mail facade import in RegistrationTest.php

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
