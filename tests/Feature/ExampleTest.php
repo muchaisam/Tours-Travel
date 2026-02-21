c188d8ad763262d788e5a16ceffa69d9d31b069c
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 11:12:06 +0300

    refactor: rename test method to follow naming conventions in ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_basic_test()
     {
         $response = $this->get('/');
 
