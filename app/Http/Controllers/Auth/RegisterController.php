cd5c7242c16328f8ff0c16305977e0c56d47b498
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 08:09:21 +0300

    feat: add default role assignment for new users during registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,6 +73,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => 'employee',
         ]);
     }
 
