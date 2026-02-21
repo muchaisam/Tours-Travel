880b98de629ad71bf14b9b71c3b812e1fe3b63a3
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 08:13:32 +0300

    feat: refactor Tag model to include factory integration and use HasFactory trait

--- app/Tag.php
+++ app/Tag.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
+use Database\Factories\TagFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-     protected $fillable = ['name'];
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    protected static function newFactory()
+    {
+        return TagFactory::new();
+    }
 
      public function destinations()
      {
