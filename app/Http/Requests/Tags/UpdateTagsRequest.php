9b84ee0e04e942f010725194d06d2b0e0bb03fd3
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 08:11:26 +0300

    fix: update authorization logic in UpdateTagsRequest to restrict access to admin users

--- app/Http/Requests/Tags/UpdateTagsRequest.php
+++ app/Http/Requests/Tags/UpdateTagsRequest.php
@@ -13,7 +13,7 @@ class UpdateTagsRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->user() && $this->user()->isAdmin();
     }
 
     /**
