8904a44707ba5a1ad6008aaf776c24201315e3b8
muchaisam <samson.muchai@strathmore.edu>
2026-02-21 08:13:12 +0300

    feat: enhance Category model with factory integration and Eloquent traits

--- app/Category.php
+++ app/Category.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
+use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name'];
+
+    protected static function newFactory()
+    {
+        return CategoryFactory::new();
+    }
 
     public function destinations()
     {
